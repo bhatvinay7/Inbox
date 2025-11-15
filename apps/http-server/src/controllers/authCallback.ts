165f67d547572b28d9efffccffdb627f19c778a1
vinay bhat <bhatvinay74@gmail.com>
2025-11-15 21:05:04 +0530

    add the google auth controller

---
+++ apps/http-server/src/controllers/authCallback.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import getUserdata from "../utils/getUserdata.js";
+const SECRET_KEY = process.env.secret_key!;
+const ACCESS_KEY= process.env.access_key!
+const callbackHandler = async (req: Request, res: Response) => {
+  try {
+    const data = await getUserdata(req, res);
+    if (!data?.email) {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
+    let user= await User.findOne({ email: data.email });
+    
+    if (!user) {
+      user = await User.create({
+        name: data.name,
+        email: data.email,
+        isEmailVerified: true,
+        picture: data.picture,
+      })
+    }
+     const refreshToken = jwt.sign(
+      {
+        username: user.name!,
+        email: user.email!,
+        userId: user._id,
+        picture:user.picture,
+        isVerified:true,
+      },
+        SECRET_KEY,
+      { expiresIn: "24d" }
+    );
+    const acces_token = jwt.sign(
+      {
+        username: user.name!,
+        email: user.email!,
+        userId: user._id,
+        picture:user.picture,
+        isVerified:true,
+    
+      },
+          ACCESS_KEY ,
+      { expiresIn: "7d" }
+    );
+    await User.findOneAndUpdate({email:user.email},{refreshToken:refreshToken})
+    
+    res.cookie("token", refreshToken , {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      domain:".shortner.services",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      path:"/"
+    });
+
+
+    res.redirect(`${process.env.NEXT_PUBLIC_FRONTEND_URL}`);
+  } catch (error: any) {
+    console.error("OAuth Error:", error.message);
+    return res.status(500).json({ message: "OAuth error", error: error.message });
+  }
+};
+
+export default callbackHandler;
---
+++ apps/http-server/src/controllers/googleauth.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import dotenv from 'dotenv'
+dotenv.config()
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
+const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI!;
+
+const googleauth= async(req:Request,res:Response)=>{
+ try{
+ const url =
+  `https://accounts.google.com/o/oauth2/v2/auth`+
+  `?client_id=${encodeURIComponent(CLIENT_ID)}`+
+  `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`+
+  `&response_type=code`+
+  `&scope=openid%20profile%20email`+
+  `&prompt=select_account`;
+  res.redirect(url);
+     }
+     catch(error:any){
+        throw new Error(error.message)
+     }
+}
+export default googleauth
