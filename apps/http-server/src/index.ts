f523af461a00bfb54818503206664707c81014c5
vinay bhat <bhatvinay74@gmail.com>
2025-11-16 23:30:25 +0530

    update index.ts file

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cluster from "cluster";
+import authRouter from "./routers/auth.router.js"
+dotenv.config();
   const app = express();
-  const PORT = process.env.PORT || 3001;
+  const PORT = process.env.PORT || 3002;
 
-  dotenv.config();
   const options = cors({
-    origin: [process.env.NEXT_PUBLIC_FRONTEND_URL!,process.env.NEXT_PUBLIC_ANALYTICS_FRONTEND_URL!],
+    origin: [process.env.NEXT_PUBLIC_FRONTEND_URL!],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   });
@@ -17,7 +17,7 @@ import cluster from "cluster";
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
   app.use(options);
+  app.use('/api',authRouter);
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-// }
