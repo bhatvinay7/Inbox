37bceda59ff6e40ecf0233dba6b07eea7501f4ea
vinay bhat <bhatvinay74@gmail.com>
2025-11-17 21:39:27 +0530

    add the client connection and reconnection for elestic search

---
+++ packages/elsesticSearch/src/index.ts
@@ -0,0 +1,37 @@
+import { Client } from "@elastic/elasticsearch";
+import dotenv from "dotenv";
+dotenv.config();
+const ELESTIC_SEARCH_CONNECTION_URL = process.env.ELESTIC_SEARCH_CONNECTION_URL!;
+const client = new Client({
+  node: ELESTIC_SEARCH_CONNECTION_URL!,
+  auth: {
+    username: "elastic",
+    password: "elestic_search_password",
+  },
+  maxRetries: 5,         
+  requestTimeout: 60000,  
+  sniffOnStart: true,
+});
+
+async function ensureConnection() {
+  let retries = 5;
+  while (retries) {
+    try {
+      await client.ping();
+      console.log("Connected to Elasticsearch");
+      return true;
+    } catch (err) {
+      retries -= 1;
+      console.error(`ES connection failed. Retrying... (${5 - retries}/5)`);
+      await new Promise((resolve,reject)=>{
+        setTimeout((resolve)=>{
+            resolve(true);
+        }, 3000)
+      });
+    }
+  }
+  console.error("Could not connect to Elasticsearch after 5 attempts.");
+  return false;
+}
+ensureConnection();
+export default client;
