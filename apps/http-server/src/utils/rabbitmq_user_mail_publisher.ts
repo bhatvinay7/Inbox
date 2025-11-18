f4b17506c2496979b0b5f08bf162d3773eb92f61
vinay bhat <bhatvinay74@gmail.com>
2025-11-18 18:14:19 +0530

    add the publisher to get mails from the api end point

--- apps/http-server/src/utils/rabbitmq_user_mail_publisher.ts
+++ apps/http-server/src/utils/rabbitmq_user_mail_publisher.ts
@@ -1,9 +1,9 @@
 import {labelQueue} from 'amqplib';
-async function pusshMessageToqueue(msg:string){
-    try{
-
-    }
-    catch(error:any){
-        console.log(error.message);
-    }
+async function pusshMessageToqueue(msg:string){     
+      try{ 
+      assigntagChannel.sendToQueue(labelQueue, Buffer.from(msg));
+     }    
+ catch(error:any){
+    throw new Error({message:`${error.message}`})
+ }
 }
