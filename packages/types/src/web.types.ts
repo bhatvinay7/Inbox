e9709015ff71c9798f3ddaf1c90546439096162d
vinay bhat <bhatvinay74@gmail.com>
2025-12-02 06:17:20 +0530

    update types

--- packages/types/src/web.types.ts
+++ packages/types/src/web.types.ts
@@ -1,11 +1,12 @@
 import {Request} from 'express'
 export interface AuthRequest extends Request {
   user?: {
-        userId: string;
-        username: string;
-        picture: string;
-        google_access_token: string;
-        isVerified: boolean;       
+        userId: string,
+        username: string,
+        email:string,
+        picture: string,
+        token:string,
+        isVerified: boolean,       
       }
 }
 
@@ -13,21 +14,27 @@ export interface userCredentials{
 userId: string,
 username: string,
 picture: string,
-google_access_token: string,
-google_refresh_token: string,
+token:string,
 email: string,
 isVerified: boolean,
-
 }
 
 export interface Mail{
-    uid:number;
-    subject:string;
-    from:string;
-    to:string[];
-    date:string;
-    body:string;
-    gmailLabels:string[];
+    userId: string,
+    uuid:string,
+    uid:number,
+    subject:string,
+    from:string,
+    to:string,
+    status: string,
+    date:string,
+    tag:string,
+    messageId:string,
+    gmailLabels?:string[],
+    raw: string,
+    body: string, 
+    attachments:string[],
+    parentMailId:string
 }
 
 export interface  queueData {
@@ -40,8 +47,54 @@ export interface google_API_SendMessage_Response{
   threadId: string,
   labelIds: [string],
   snippet: string,
-  historyId: strstring
+  historyId: string,
   internalDate: string,
   sizeEstimate:number,
   raw: null
 }
+
+export interface userData{
+  userId:string,
+  user_email:string,
+  accessToken: string
+}
+
+export interface attachment{
+  uuid:string,
+  link:string,
+  progress:string,
+  status?:string
+}
+
+interface file{
+    uuid:string,
+    isLoading:boolean,
+    error:boolean,
+    link:string,
+
+}
+interface mail{
+  to:string,
+  subject:string,
+  body:string
+}
+
+export type GoogleUser = {
+  id: string;
+  email: string;
+  verified_email: boolean;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  token_type: string;
+  id_token: string;
+};
+
+export type SelectedUser = Pick<
+  GoogleUser,
+  "access_token" | "name" | "refresh_token" | "picture"| "email"
+>;
