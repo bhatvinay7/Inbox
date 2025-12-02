04cc6fa48163fee2cdaa6563d248cbe33a1a7ca7
vinay bhat <bhatvinay74@gmail.com>
2025-12-02 06:28:42 +0530

    update the mail consumer and mail publisher

--- apps/label-assigner/src/utils/EmaillConsumer.ts
+++ apps/label-assigner/src/utils/EmaillConsumer.ts
@@ -1,15 +1,16 @@
-import {assigntagChannel} from 'rabbitmq';
+import {imapmailChannel,ConsumeMessage} from 'rabbitmq';
 import pushMessageToqueue from './pblish_mailTo_worker_queue.js';
 import assignLabel from './agent.js';
 import {queueData} from 'types'
 export async function consumeMessage(queueName:string){
   try{
-    await assigntagChannel.consume(queueName, async(msg) => {
+    await imapmailChannel.consume(queueName, async(msg:ConsumeMessage|null) => {
     if (msg !== null) {
      const {message,userId}=JSON.parse(msg.content.toString()) as queueData
-     const response=await assignLabel(message); 
-     await pushMessageToqueue(JSON.stringify({message:{...message,tag:response},userId:userId}));
-      assigntagChannel.ack(msg);
+     const response=await assignLabel(JSON.stringify(message.body!)); 
+     await pushMessageToqueue(JSON.stringify({message:{...message,gmailLabels:[...[message.gmailLabels?.map((a:string)=>a)],response]},userId:userId}));
+     console.log(JSON.stringify({message:{...message,gmailLabels:[...[message.gmailLabels?.map((a:string)=>a)],response]},userId:userId}))
+      imapmailChannel.ack(msg);
     } else {
       console.log('Consumer cancelled by server');
     }
--- apps/label-assigner/src/utils/pblish_mailTo_worker_queue.ts
+++ apps/label-assigner/src/utils/pblish_mailTo_worker_queue.ts
@@ -1,7 +1,7 @@
-import {sendMail,labelAssignedQueue} from "rabbitmq";
+import {storeMail,labelAssignedQueue} from "rabbitmq";
 async function pushMessageToqueue(message:string){
     try{
-  sendMail.sendToQueue(labelAssignedQueue, Buffer.from(message));
+     storeMail.sendToQueue(labelAssignedQueue, Buffer.from(message));
     }
     catch(error:any){
         console.log(error.message);
